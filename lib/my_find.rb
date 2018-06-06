5dec92d00b5167e503e0e57991b32263d773763b
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-06 02:47:17 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,10 +2,8 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  #new_empty_array
   while i < collection.length
     return collection[i] if yield(collection[i])
-    end
     i += 1
   end
 end
