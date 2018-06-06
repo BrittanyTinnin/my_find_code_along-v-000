1b1964bb84193fd68c2457f4d4e85982c159d30b
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-06 02:36:53 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,9 @@ def my_find(collection)
   i = 0
   #new_empty_array
   while i < collection.length
-    yield(collection[i])
+    if yield(collection[i])
+      return collection[i]
+    end
     i += 1
   end
 
